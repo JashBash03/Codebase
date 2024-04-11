3f26b81bc4b426191a361d3dfdcfb93ef24fa3c6
Javi <javiercamarerolopez@gmail.com>
2024-04-11 17:12:15 +0200

    Build Paint multiplayer

--- socketio/index.js
+++ socketio/index.js
@@ -11,9 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
+app.get('/paint', (req, res) => {
+  res.sendFile(join(__dirname, 'paint.html'));
+});
+
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
+    });
+    socket.on('paint', (datos) => {
+      io.emit('paint', datos);
     });
 });
 
